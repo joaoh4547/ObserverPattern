93aed22dca2462f0c7da60ced8a4a1b5e7b50829
João Henrique <joaohenriquetdj@gmail.com>
2021-11-15 15:27:46 -0300

    add IObserver and ISubject

--- ObserverPattern/Program.cs
+++ ObserverPattern/Program.cs
@@ -7,6 +7,8 @@ namespace ObserverPattern
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine();
+           
         }
     }
 }
