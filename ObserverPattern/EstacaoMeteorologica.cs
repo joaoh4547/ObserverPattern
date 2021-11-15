2890994d071dfe2954c106ee37455d43d2f7cf60
João Henrique <joaohenriquetdj@gmail.com>
2021-11-15 15:39:32 -0300

    add EstacaoMeteorologica

---
+++ ObserverPattern/EstacaoMeteorologica.cs
@@ -0,0 +1,40 @@
+﻿using ObserverPattern.Interfaces;
+using System.Collections.Generic;
+
+namespace ObserverPattern
+{
+    public class EstacaoMeteorologica : ISubject
+    {
+        private List<IObserver> observers;
+        private double _temperatura;
+        public double temperatura
+        {
+            get
+            {
+                return _temperatura;
+
+            }
+            set
+            {
+                _temperatura = value;
+                Notificar();
+            }
+        }
+        public EstacaoMeteorologica()
+        {
+            observers = new List<IObserver>();
+        }
+        public void Anexar(IObserver observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void Notificar()
+        {
+            observers.ForEach(observer =>
+            {
+                observer.Atualizar(this);
+            });
+        }
+    }
+}
