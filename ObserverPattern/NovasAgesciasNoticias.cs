4f33e4c1a1bbc04d79e54d47e6f458b68bc0bfc9
João Henrique <joaohenriquetdj@gmail.com>
2021-11-15 15:55:22 -0300

    add  NovasAgesciasNoticias

---
+++ ObserverPattern/NovasAgesciasNoticias.cs
@@ -0,0 +1,24 @@
+﻿using ObserverPattern.Interfaces;
+using System;
+
+namespace ObserverPattern
+{
+    public class NovasAgesciasNoticias : IObserver
+    {
+        public string NomeAgencia { get; set; }
+        public NovasAgesciasNoticias(string NomeAgencia)
+        {
+            this.NomeAgencia = NomeAgencia;
+        }
+        public void Atualizar(ISubject subject)
+        {
+            if (subject is EstacaoMeteorologica estacao)
+            {
+                var temp = estacao.temperatura;
+                Console.WriteLine($"{NomeAgencia} reportando a temperatura {temp} graus celcius");
+                Console.WriteLine("-------------------------------");
+            }
+            return;
+        }
+    }
+}
